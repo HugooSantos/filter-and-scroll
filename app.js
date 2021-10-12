d3cb4a6776b7c4d8cf25b146f30e6f8aed9a48b4
Hugo Gabriel <hugogabrieltjj@gmail.com>
2021-10-12 11:37:50 -0300

    commit to finish code and up to github

--- app.js
+++ app.js
@@ -1,48 +1,78 @@
-const postsContainer = document.querySelector('#posts-container')
-const loaderContainer = document.querySelector('.loader')
+const postsContainer = document.querySelector('#posts-container');
+const filterInput = document.querySelector('#filter');
+const containerLoader = document.querySelector('.loader');
 
-let page = 1
+let page = 1;
 
-const getPosts = async () => {
-    const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=5&_page=${page}`)
-    return response.json()
+const getPost = async () => {
+  const response = await 
+    fetch(`https://jsonplaceholder.typicode.com/posts?_limit=5&_page=${page}`);
+  return response.json();
 }
 
-const addPostsIntoDOM = async () => {
-    const posts = await getPosts()
-    const postsTemplate = posts.map(({ id, title, body }) => `
-    <div class="post">
-        <div class="number">${id}</div>
-        <div class="post-info>
-            <h2 class"post-title">${title}</h2>
-            <p class="post-body">${body}</p>
-        </div>
-    </div>
-    `).join('')
-    postsContainer.innerHTML += postsTemplate
-}
-addPostsIntoDOM()
-const getNextPosts = () => {
-    page++
-    addPostsIntoDOM()
+const addPostIntoDom = async ()  => {
+  const posts = await getPost();
+  const postsTemplate = 
+  posts.map(({id, title, body}) =>
+       `
+      <div class="post">
+      <div class="number">${id}</div>
+      <div class="post-info">
+        <h2 class="post-title">${title}</h2>
+        <p class="post-body">${body}</p>
+      </div>
+      </div>
+      `
+      ).join('')
+      postsContainer.innerHTML += postsTemplate;
 }
 
-const removeLoader = () => {
-    setTimeout(() => {
-        loaderContainer.classList.remove('show')
-        getNextPosts()
-    }, 1000)    
+
+const getNextsPosts = () =>{
+  setTimeout(() =>{
+    page++;
+    addPostIntoDom();
+  },300)
 }
 
+const removeLoader = () =>{
+  setTimeout( () =>{
+    containerLoader.classList.remove('show');
+    getNextsPosts();
+  },1000)
+}
 
 const showLoader = () => {
-    loaderContainer.classList.add('show')
-    removeLoader()
+  containerLoader.classList.add('show');
+  removeLoader();
 }
+
 window.addEventListener('scroll', () => {
-    const { clientHeight, scrollHeight, scrollTop } = document.documentElement
-    const isPageBottomAlmostReached = scrollTop + clientHeight > scrollTop -10
-    if (isPageBottomAlmostReached) {
-        showLoader()
-    }
+  
+  const {scrollTop, clientHeight, scrollHeight} = document.documentElement;
+  const isPageBottomAlmostReached = scrollTop + clientHeight >= scrollHeight - 10;
+  if(isPageBottomAlmostReached){
+    showLoader();
+    
+  }
 })
+
+filterInput.addEventListener('input', event => {
+  const inputValue = event.target.value.toLowerCase();
+  const posts = document.querySelectorAll('.post');
+  
+  posts.forEach(post => {
+    const postTitle = post.querySelector('.post-title').textContent.toLowerCase();
+    const postBody = post.querySelector('.post-body').textContent.toLowerCase();
+    const inputContainsValue = postTitle.includes(inputValue) 
+    || postBody.includes(inputValue);
+    
+    if(inputContainsValue){
+      post.style.display = 'flex';
+      return;
+    }
+    post.style.display = 'none';
+  })
+})
+
+addPostIntoDom();
